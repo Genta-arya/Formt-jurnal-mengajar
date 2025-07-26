831922155e9f953b5a1412c737d3b28527d696b6
Genta-arya <mgentaaryap@gmail.com>
2025-07-26 14:59:27 +0700

    update meta data

--- src/Views/Form.jsx
+++ src/Views/Form.jsx
@@ -559,7 +559,7 @@ const JurnalMengajarForm = () => {
 
           {/* Tombol Submit */}
         </div>
-        <div className="max-w-3xl bg-[#f4f1ee] mx-auto  mt-4 text-center font-bold items-center flex justify-between">
+        <div className="max-w-3xl bg-[#f4f1ee] mx-auto  mt-4 text-center font-bold items-center flex flex-wrap justify-between">
           <button
             type="submit"
             onClick={handleSubmit}
@@ -570,7 +570,7 @@ const JurnalMengajarForm = () => {
 
           <button
             onClick={handleClearForm}
-            className="border border-gray-400 rounded  px-6 py-2 ml-2 text-blue-700 w-full hover:opacity-80 font-bold"
+            className="border border-gray-400 rounded text-xs  py-2.5 ml-2 text-blue-700 w-full hover:opacity-80 font-bold"
           >
             Kosongkan Formulir
           </button>
